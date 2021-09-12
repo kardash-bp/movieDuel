7a50c2a25c497002cb3a4b326e14035a7a8b49f7
kardash-bp <kardasbp@gmail.com>
2021-09-12 12:59:39 +0200

    get momie data from api with axios

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+import { api } from './config.js'
+const getData = async () => {
+  const res = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: api.key,
+      s: 'avengers',
+    },
+  })
+  console.log(res.data)
+}
+getData()
